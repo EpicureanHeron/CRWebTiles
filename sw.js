a50c9068d09848adda9aa443bb13a048d97373da
Joe Thor <jfthor@umn.edu>
2018-07-09 15:39:38 -0500

    manifest updated. playing around with service worker, trying to figure this out

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '../../index.html'
+       '/index.html'
      ]);
    })
  );
