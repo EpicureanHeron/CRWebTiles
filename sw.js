534d5500a23730e5f0fa0f3f0654de8b1c1c0265
EpicureanHeron <joethor1989@gmail.com>
2018-07-10 15:59:18 -0500

    changed the manifest file to have index.html rather than the chromeriver SSO. Changed the service worker's cache.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('video-store').then(function(cache) {
+   caches.open('CR').then(function(cache) {
      return cache.addAll([
        '/index.html'
      ]);
